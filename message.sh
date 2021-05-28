96ac8b40a471bd5193bae93a5e97aada1520c767
杨建 <1107858970@qq.com>
2021-05-28 22:51:31 +0800

    test

--- message.sh
+++ message.sh
@@ -5,7 +5,7 @@ echo dayofweek_begin=$dayofweek
 
 if [ $dayofweek -eq 0 ] || [ $dayofweek -eq 6 ]
 then
-    
+    echo "周末"
 else
     curl --form-string "text=hello" "https://api.chanify.net/v1/sender/CIDKi4kGEiJBQkRKTFBDUVFKSjVPVlgzSjNYWE1SU0VITlNTSkxNWEpFGhSJSjvVdNUJ4SfCY617cnvNkEaC9SIECAEQAQ.DiQycwpdyWIMB5fyHp2STrbIIK6rSknJ8RnrKCTRC6Q"
 fi
